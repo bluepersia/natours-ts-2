bc032c7bad85594187f2da7c620bf436fcef8faf
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-05-24 11:34:05 -0600

    Tour CRUD

---
+++ src/routes/tourRoutes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router ();
+import tourController = require ('../controllers/tourController');
+
+router.route ('/').get (tourController.getAllTours).post (tourController.createTour);
+router.route ('/:id').get (tourController.getTour).patch (tourController.updateTour).delete (tourController.deleteTour);
+
+export default router;
