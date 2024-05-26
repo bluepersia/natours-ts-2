ad4386c3f24e6e05d6fe3f24cc9751603ca2fbd0
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-05-26 11:07:06 -0600

    Update Me

---
+++ src/controllers/userController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+import handle from 'express-async-handler';
+import { IRequest } from "./authController";
+
+
+
+export const updateMe = handle (async(req:Request, res:Response):Promise<void> =>
+{
+    let update:{[key:string]:any} = {};
+
+    ['name', 'email', 'photo'].forEach (el => {
+        if (req.body.hasOwnProperty (el))
+            update[el] = req.body[el];
+    })
+
+    const user = await User.findByIdAndUpdate ((req as IRequest).user.id, update, {new:true, runValidators:true});
+
+    res.status (200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    })
+
+});
