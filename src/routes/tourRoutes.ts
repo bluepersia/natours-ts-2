88377cbbfc58b0cabbfe1267c71274990e175158
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-05-26 11:58:06 -0600

    Nested Routes

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 const router = express.Router ();
+import reviewRouter from './reviewRoutes';
 import tourController = require ('../controllers/tourController');
+
+router.use ('/:tourId/reviews', reviewRouter);
 
 router.route ('/').get (tourController.getAllTours).post (tourController.createTour);
 router.route ('/:id').get (tourController.getTour).patch (tourController.updateTour).delete (tourController.deleteTour);
