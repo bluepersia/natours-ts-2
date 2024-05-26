14a7c249fb90e9f22f704c1c7a60c88ffb91d071
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-05-26 10:53:10 -0600

    Forgot Password

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,4 +7,6 @@ const upload = multer ();
 router.post ('/sign-up', upload.none (), authController.signup);
 router.post ('/log-in', upload.none (), authController.login);
 
+router.post ('/forgot-password', upload.none(), authController.forgotPassword);
+
 export default router;
