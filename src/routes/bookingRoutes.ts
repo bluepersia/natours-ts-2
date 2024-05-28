f6f1d70bc35217ff731adba79b8c28b340feb7e5
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-05-28 10:25:45 -0600

    Bookings CRUD

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -7,5 +7,9 @@ router.use (authController.protect);
 
 router.get ('/stripe-checkout-session/:tourId', bookingController.getStripeCheckoutSession);
 
+router.use (authController.restrictTo('admin', 'lead-guide'));
+
+router.route ('/').get (bookingController.getAllBookings).post (bookingController.createBooking);
+router.route ('/:id').get (bookingController.getBooking).patch (bookingController.updateBooking).delete (bookingController.deleteBooking);
 
 export default router;
