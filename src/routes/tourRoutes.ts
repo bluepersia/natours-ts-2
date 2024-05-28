102602b4ab8ff7465a508df6778af8e84f944722
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-05-28 10:08:49 -0600

    Upload tour images

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -6,7 +6,7 @@ import authController = require ('../controllers/authController');
 
 router.use ('/:tourId/reviews', reviewRouter);
 
-router.route ('/').get (tourController.getAllTours).post (authController.protect, authController.restrictTo('lead-guide', 'admin'), tourController.createTour);
-router.route ('/:id').get (tourController.getTour).patch (authController.protect, authController.restrictTo('lead-guide', 'admin'), tourController.updateTour).delete (authController.protect, authController.restrictTo('lead-guide', 'admin'), tourController.deleteTour);
+router.route ('/').get (tourController.getAllTours).post (authController.protect, authController.restrictTo('lead-guide', 'admin'), tourController.uploadImages, tourController.processImages, tourController.createTour);
+router.route ('/:id').get (tourController.getTour).patch (authController.protect, authController.restrictTo('lead-guide', 'admin'), tourController.uploadImages, tourController.processImages, tourController.updateTour).delete (authController.protect, authController.restrictTo('lead-guide', 'admin'), tourController.deleteTour);
 
 export default router;
